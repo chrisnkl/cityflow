0ff2d50802aaa601596169100d6eb7613a63f233
chrisnkl <chrinikolaou.software@gmail.com>
2026-08-28 23:18:29 +0300

    init commit

---
+++ cf-backend/src/main/java/com/chrisnkl/cfbackend/CfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chrisnkl.cfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfBackendApplication.class, args);
+    }
+
+}
